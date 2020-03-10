7658394395c56a384354c05438dcc9907c4a7b74
Scott McLeod <sdm@Lauras-MacBook-Pro.local>
2020-03-09 20:55:33 -0700

    broke my game

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -24,20 +24,35 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch() {
+	src.setAttribute(cards[cardId].cardImage);
+	if (cardsInPlay >=2) {
 	if (cardsInPlay[0] ===cardsInPlay[1]) {
 		console.log("You have a match!");
 	} else {
 		console.log("Sorry, try again.");
 	}
 }
+}
 
-function flipCard (cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
-flipCard(0);
-flipCard(2);
 
 console.log(cardImage);
 console.log(suit);
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', 'flipCard');
+		document.querySelector('cardElement').appendChild('game-board');
+	}
+}
+
+createBoard();
+
